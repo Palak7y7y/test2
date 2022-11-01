0a75651c621853a48b1cb47f4169961e84068b26
Shivangi2323 <shivangileadorigin@gmail.com>
2022-11-01 15:48:30 +0530

    new commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,7 +20,7 @@
                             <main class="col-sm-10 col-sm-offset-1">
                                 <div class="post_details">
                                     <h3>It is a long established fact that a reader will be distracted. </h3>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+                                    <p>Lorem Ipsum is simplklbj;y dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
                                         text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has 
                                         survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                                     <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
